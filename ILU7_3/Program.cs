ecd77d49ab236ab8c5af80c8a6f993058b3d5717
Keli06001 <keli06001@utb.vaxjo.se>
2024-02-02 13:22:05 +0100

    Add project files.

---
+++ ILU7_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ILU7_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> area = new Dictionary<string, int>();
+            area.Add("Sverige", 450);
+            area.Add("Norge", 385);
+            area.Add("Danmark", 43);
+            area.Add("Finland", 338);
+            Console.WriteLine("Skriv in antingen Sverige, Norge, Danmark eller finland för att få reda på dess area");
+            string land=Console.ReadLine();
+            
+            foreach(KeyValuePair<string, int> kvp in area) 
+            {
+                if (land == kvp.Key)
+                {
+                    Console.WriteLine(kvp.Value+"*10^5km^2");
+                }
+            }
+            
+
+
+        }
+    }
+
+}
